8a092eecb6b2fa5868cb6d9440c4cc2cdcfa1441
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2022-07-06 09:59:51 -0300

    atualização do exemplo de configuração

--- const.example.php
+++ const.example.php
@@ -1,6 +1,7 @@
 <?php
 
-define('DEFAULT_LANGUAGE', 'en');
+define('DISPLAY_ERRORS', true);
+define('DEFAULT_LANGUAGE', 'pt');
 define('MAINTENANCE_MODE', 0);
 define('MYSQL_HOST', 'localhost');
 define('MYSQL_DATABASE', 'twitcode');
@@ -8,5 +9,13 @@ define('MYSQL_USERNAME', 'root');
 define('MYSQL_PASSWORD', '');
 define('MYSQL_PORT', '3306');
 define('SITE_LANGUAGES', 'en,pt');
+define('SITE_EMAIL', 'noreply@twitcode.com');
 define('SITE_NAME', 'TwitCode');
 define('SITE_URL', 'http://localhost');
+define('SMTP_AUTH', false);
+define('SMTP_DEBUG', false);
+define('SMTP_HOST', 'localhost');
+define('SMTP_PORT', '2525');
+define('SMTP_TYPE', 'smtp');
+define('SMTP_USER', '');
+define('SMTP_PASSWORD', '');
